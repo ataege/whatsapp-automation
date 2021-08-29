c064f85a468ee53430fa0876eb3ba49c623aff3a
Ata Ege Asiltürk <ataegeasilturk@gmail.com>
2021-08-29 20:38:24 +0300

    date checker test

---
+++ tests/date_checker.spec.js
@@ -0,0 +1,8 @@
+import { checkDate } from '../src/utils/dateChecker';
+
+describe('checkDate', () => {
+	it('should return boolean', () => {
+		var checkedDate = checkDate('August 29, 2021 15:50:00');
+		expect(typeof checkedDate === 'boolean').toBeTruthy();
+	});
+});
